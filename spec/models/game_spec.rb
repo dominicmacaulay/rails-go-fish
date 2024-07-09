7b196702574c7b14df69dcd5d264d5a1e14d8854
dominicmacaulay <dominic.62502@gmail.com>
2024-07-09 16:44:23 -0400

    make different layout for the landing page and all devise pages

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  describe 'associations' do
-    let(:game) { create(:game) }
-    let(:user1) { create(:user) }
-    let(:user2) { create(:user) }
-
-    it 'it has users when they are added to the game' do
-      game.users << user1
-      game.users << user2
-      expect(game.users).to include(user1, user2)
-    end
-  end
-
   describe 'queue_full' do
     let!(:game) { create(:game) }
     let!(:user1) { create(:user) }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'associations' do
-    let(:user) { create(:user) }
-    let(:game1) { create(:game) }
-    let(:game2) { create(:game) }
-
-    it 'it has users when they are added to the game' do
-      user.games << game1
-      user.games << game2
-      expect(user.games).to include(game1, game2)
+  describe 'name' do
+    let(:user) { create(:user, email: 'dominic@gmail.com') }
+    it 'returns the capitalized name based off of the email' do
+      expect(user.name).to eql 'Dominic'
     end
   end
 end
