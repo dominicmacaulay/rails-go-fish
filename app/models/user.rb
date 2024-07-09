7b1ea18cc1f52ace206b8b4bf4361701bce03cdd
dominicmacaulay <dominic.62502@gmail.com>
2024-07-09 16:04:47 -0400

    fix simple form and flash message issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def name
-    email.split('@').first.capitalize
+    first_split = email.split('@').first.capitalize
+    first_split.split('.').first
   end
 end
